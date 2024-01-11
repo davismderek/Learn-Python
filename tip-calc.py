9a4def185c23d6d59dcf39fc560ca428d76dbcb7
Derek Davis <davismderek@gmail.com>
2024-01-11 10:16:46 -0500

    Revised and simplified tip-calc with cleaner code

--- tip-calc.py
+++ tip-calc.py
@@ -1,26 +1,18 @@
 bill = input("Total bill amount? ")
 servicelevel = input("Level of service? Please write 'Good', 'Fair', or 'Bad': ")
+tip = 0
+
 if servicelevel == "Good":
     tip = (float(bill)) * (float(.20))
-    tiprounded = ("%.2f" % tip)
-    total = tip + float(bill) 
-    totalrounded = ("%.2f" % total)
-    print("Tip Amount: $" + (str(tiprounded)))
-    print("Total Amount: $" + str(totalrounded))
-
 
 elif servicelevel == "Fair":
-    tip = (float(bill)) * (float(.15))
-    tiprounded = ("%.2f" % tip)
-    total = tip + float(bill)
-    totalrounded = ("%.2f" % total)
-    print("Tip Amount: $" +  (str(tiprounded)))
-    print("Total Amount: $" + str(totalrounded))
+    tip = (float(bill)) * (float(.15))   
 
 elif servicelevel == "Bad":
     tip = (float(bill)) * (float(.10))
-    tiprounded = ("%.2f" % tip)
-    total = tip + float(bill)
-    totalrounded = ("%.2f" % total)
-    print("Tip Amount: $" + (str(tiprounded)))
-    print("Total Amount: $" + str(totalrounded))
+
+tiprounded = ("%.2f" % tip)
+total = tip + float(bill)
+totalrounded = ("%.2f" % total)
+print("Tip Amount: $" + (str(tiprounded)))
+print("Total Amount: $" + str(totalrounded))
