26f412c4cd99647ad42cee6429bc91607b3a79f5
Derek Davis <davismderek@gmail.com>
2024-01-16 08:58:58 -0500

    moved commented out code to bottom. instructions up top

--- word_histogram.py
+++ word_histogram.py
@@ -10,32 +10,58 @@
 
 
 def word_summary(sentence):
-# this function takes a single argument "sentence"
     words = sentence.lower().split()
-    # this variable word makes sentence lowercase and splits string into a list, making each word individual
     hist_dictionary = {}
-    # this creates a blank dictinary for us to store all these words in it
     for word in words: 
-    # for loop to find each word in words/sentence input
         word = word.strip(".,?!")
-        # removes extra special characters
         if word.isalpha():
-        # if conditional statement to make it all alphabetic characters
             if word in hist_dictionary:
-            # if word is in hist_dict then....
                 hist_dictionary[word] += 1
-                # if its there already, then add one more count to it 
             else :
-            # if its not there...
                 hist_dictionary[word] = 1
-                # if its not there then give it a count 
     return hist_dictionary
-    # returns the dictionary containing the count of each word 
 
 user_input = input("Type a sentence: ")
-# what user types in
 result = word_summary(user_input)
-# calls the word_summary function to the user_input and assigns this to variable result
 print(result)
-# prints the result of the function 
 
+
+
+
+
+
+
+
+
+
+
+
+# def word_summary(sentence):
+# # this function takes a single argument "sentence"
+#     words = sentence.lower().split()
+#     # this variable word makes sentence lowercase and splits string into a list, making each word individual
+#     hist_dictionary = {}
+#     # this creates a blank dictinary for us to store all these words in it
+#     for word in words: 
+#     # for loop to find each word in words/sentence input
+#         word = word.strip(".,?!")
+#         # removes extra special characters
+#         if word.isalpha():
+#         # if conditional statement to make it all alphabetic characters
+#             if word in hist_dictionary:
+#             # if word is in hist_dict then....
+#                 hist_dictionary[word] += 1
+#                 # if its there already, then add one more count to it 
+#             else :
+#             # if its not there...
+#                 hist_dictionary[word] = 1
+#                 # if its not there then give it a count 
+#     return hist_dictionary
+#     # returns the dictionary containing the count of each word 
+
+# user_input = input("Type a sentence: ")
+# # what user types in
+# result = word_summary(user_input)
+# # calls the word_summary function to the user_input and assigns this to variable result
+# print(result)
+# # prints the result of the function 
